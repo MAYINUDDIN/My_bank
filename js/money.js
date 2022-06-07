8127d611c697e76634296c7d6e2da4f5de2c5244
MAYINUDDIN <mainu768971@gmail.com>
2022-06-07 10:39:17 +0600

    first commit

--- js/money.js
+++ js/money.js
@@ -6,16 +6,21 @@ document.getElementById("calculate").addEventListener("click", function () {
     let rent = document.getElementById("rent").value;
     let cloths = document.getElementById("cloths").value;
 
+    //summation total expense
     let sum = parseInt(food) + parseInt(rent) + parseInt(cloths);
-    const total = document.getElementById("t");
+    const total = document.getElementById("totalExpense");
     total.innerText = sum;
 
-    const summ = parseInt(income) - sum;
-    const totall = document.getElementById("balance");
-    totall.innerText = summ;
+    const sumBalance = parseInt(income) - sum;
+    const totalBalance = document.getElementById("balance");
+    totalBalance.innerText = sumBalance;
 
+
+    //clear input data
+    income.value = '';
+    savingsAmount.value = '';
     food.value = '';
-    rent.value = 'dfg';
+    rent.value = '';
     cloths.value = '';
 
 })
@@ -24,19 +29,24 @@ document.getElementById("saveAmount").addEventListener("click", function () {
     var income = document.getElementById("myIncome").value;
     let savingsAmount = document.getElementById("savingsAmount").value;
     let balance = document.getElementById("balance").innerText;
+    //Find Savings Amount
     let savingsSum = parseInt(income) / 100 * savingsAmount;
     const savingAmt = document.getElementById("saveingAmt");
     savingAmt.innerText = savingsSum;
+    //Find Savings Amount  
 
-
+    // finding remaining balance
     let remainingsum = balance - savingsSum;
     const remaingAmt = document.getElementById("remaingBalance");
     remaingAmt.innerText = remainingsum;
+    // finding remaining balance
 
+    //clear input data
+    income.value = '';
+    savingsAmount.value = '';
     food.value = '';
     rent.value = '';
     cloths.value = '';
-
 })
 
 
